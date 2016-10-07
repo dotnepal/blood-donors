e041cfcf6e34f855e249377381c4df2cd5d24d96
Samundra Shrestha <samundras@rabbit.co.th>
2016-10-08 03:26:03 +0700

    add dummy nginx configuration

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -13,7 +13,23 @@ class ExampleTest extends TestCase
      */
     public function testBasicExample()
     {
-        $this->visit('/')
-             ->see('Laravel');
+        $this->visit('http://blood-donor.dev/')->see('Laravel');
+    }
+
+    protected function makeRequest($method, $uri, $parameters = [], $cookies = [], $files = [])
+    {
+        $uri = $this->prepareUrlForRequest($uri);
+
+        $this->call($method, $uri, $parameters, $cookies, $files);
+
+        $this->clearInputs()->followRedirects()->assertPageLoaded($uri);
+
+        $this->currentUri = $this->app->make('request')->fullUrl();
+
+        dd("Current URL", $this->currentUri);
+
+        $this->crawler = new Crawler($this->response->getContent(), $this->currentUri);
+
+        return $this;
     }
 }
