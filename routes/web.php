1033d904bcbbf25f9a0dcc71e15fbac827419246
Samundra Shrestha <samundra.shr@gmail.com>
2016-10-09 01:23:48 +0700

    Integrate Travis-CI for the tests, deployment (#9)
    * comment curl code
    
    * add networking in travis config
    
    * add .env.travis
    
    * remove unnecessary calls

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,4 @@
 |
 */
 
-Route::get('/', 'SiteController@home');
-Route::get('/signup','SiteController@signup')->name('signup');
-Route::get('/login','SiteController@login')->name('login');
-Route::post('/login','SiteController@login_do')->name('login.do');
-Route::post('/signup','SiteController@signup_do')->name('signup.do');
+Route::get('/', 'SiteController@index');
