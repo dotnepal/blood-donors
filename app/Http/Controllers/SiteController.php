157f5bc2d436db5d35e5f21ad91d0bed8e98fa42
Samundra Shrestha <samundras@rabbit.co.th>
2016-10-09 00:30:11 +0700

    remove unnecessary calls

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -3,34 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
-use App\Http\Requests;
+use View;
 
 class SiteController extends Controller
 {
-    public function home(){
-      //Returning home page
-      return \View::make('site.home');
+    public function index() {
+      return View::make('welcome');
     }
-
-    public function login(){
-
-      return \View::make('site.login');
-    }
-
-    public function signup(){
-
-      return \View::make('site.signup');
-    }
-
-    public function login_do(Request $request){
-
-      //
-    }
-
-    public function signup_do(Request $request){
-
-      //
-    }
-
 }
