533fc8053c6d9bca15dd14c274d77e8da188cf27
Swornim Srestaa <srestaswrnm@gmail.com>
2016-10-10 08:37:24 +0545

    Views delted

--- resources/views/site/login.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('title','Login')
-
-@section('body')
-<h3> This is login page</h3>
-
-@stop
--- resources/views/site/signup.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('title','Create an account')
-
-@section('body')
-<h3> This is signup page </h3>
-@stop
