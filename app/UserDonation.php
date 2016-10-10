5f674ab6359bbc989c8d826537c0fbf5429dcfb5
Swornim Srestaa <srestaswrnm@gmail.com>
2016-10-10 18:56:29 +0545

    Migrations and Seeders

---
+++ app/UserDonation.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserDonation extends Model
+{
+    protected $table = "user_donation";
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(UsersTableSeeder::class);
+        $this->call(DonationsTableSeeder::class);
+        $this->call(UserDonationTableSeeder::class);
     }
 }
