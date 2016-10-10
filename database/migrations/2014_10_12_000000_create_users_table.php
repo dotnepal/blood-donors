a30a51c0316be58782dd980229c7257932e74bcb
Swornim Srestaa <srestaswrnm@gmail.com>
2016-10-10 11:24:47 +0545

    Migrations done

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
+            $table->string('username')->unique();
             $table->string('password');
+            $table->bigInteger('phone');
+            $table->longText('address');
+            $table->string('blood_group');
+            $table->integer('user_type_id');
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2016_10_10_053347_create_donations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDonationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('donations',function(Blueprint $table){
+        $table->increments('id');
+        $table->integer('donor_id');
+        $table->integer('receiver_id')
+        $table->timestamps();
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
