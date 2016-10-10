7e0af239edbe29f989b79e36d1fb2d2202cfb5af
Swornim Srestaa <srestaswrnm@gmail.com>
2016-10-10 22:02:40 +0545

    Localization

---
+++ resources/lang/en/general.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+
+  "email" => "Email",
+  "login" => "Login",
+  "logout" => "Logout",
+  "password" => "Password",
+  "password_confirm" => "Confirm Password",
+  "remember_me" => "Remeber Me",
+  "reset" => "Reset Password",
+  "signup" => "Create an account".
+  "reset_link" => "Send Password Reset Link",
+
+);
+
+
+ ?>
