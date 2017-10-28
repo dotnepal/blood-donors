b6b93136b031c61ebad0dec383443a28e6f5e93c
Samundra Shrestha <samundra.shr@gmail.com>
2017-10-28 17:29:52 +0700

    PATH-26 add docker development setup

--- init.sh
+++ init.sh
@@ -7,3 +7,6 @@ cp docker-compose.yml.dist docker-compose.yml
 (cd dockerfiles && docker build .)
 
 docker-compose up -d
+
+docker exec -it blooddonors_fpm_1 composer install
+docker exec -it blooddonors_fpm_1 composer --version
